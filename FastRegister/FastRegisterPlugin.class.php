3e9286d674b5442070654fb1b541243eeb7791e2
Hitman007 <support@customrayguns.com>
2016-10-09 12:57:14 -0700

    xx

--- FastRegister/FastRegisterPlugin.class.php
+++ FastRegister/FastRegisterPlugin.class.php
@@ -6,7 +6,7 @@ class FastRegisterPlugin{
 	
 	public function __construct(){
 		$this->doAutoloader();
-		$SidebarWidget = new SidebarWidget();
+		//$SidebarWidget = new SidebarWidget();
 	}
 	
 	public function doAutoloader(){
