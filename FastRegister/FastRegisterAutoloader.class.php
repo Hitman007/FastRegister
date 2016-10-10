91bc2a9d2e7c2283cfca41e2e9698aabd1f3b914
Hitman007 <support@customrayguns.com>
2016-10-10 12:41:58 -0700

    Update FastRegisterAutoloader.class.php

--- FastRegister/FastRegisterAutoloader.class.php
+++ FastRegister/FastRegisterAutoloader.class.php
@@ -6,12 +6,12 @@ class FastRegisterAutoloader{
 	
 	public function __construct(){
 		spl_autoload_register(function ($className){
-			echo (substr($className, 0, 13)) . "<br />"; 
-			$includePath = substr($className, 13);
-			
-			$includePath = dirname(__DIR__) . "/FastRegister/" . $includePath . ".class.php";
-			//var_dump($includePath);die();
-			include $includePath;
+			$front =  (substr($className, 0, 12));
+			if ($front == "FastRegister"){
+				$includePath = substr($className, 13);
+				$includePath = dirname(__DIR__) . "/FastRegister/" . $includePath . ".class.php";
+				include $includePath;
+			}
 		});
 	}
 	
