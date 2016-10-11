b4a0216250435b4f1076491a13a3ffcd5f1fd9b6
Hitman007 <support@customrayguns.com>
2016-10-10 23:15:56 -0700

    Update FastRegister.php

--- FastRegister.php
+++ FastRegister.php
@@ -15,5 +15,5 @@ require "FastRegister/FastRegisterPlugin.class.php";
 $FastRegisterPlugin = new FastRegisterPlugin;
 
 $FastRegisterPlugin->enableShortCode_fastRegister();
-$FastRegisterPlugin->enableSidebarWidget()'
+$FastRegisterPlugin->enableSidebarWidget();
 $FastRegisterPlugin->enableFormListener();
